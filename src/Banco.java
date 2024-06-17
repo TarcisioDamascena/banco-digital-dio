f094792014010a97bf39b3cc2928d183222ba503
DamasX <84998412+TarcisioDamascena@users.noreply.github.com>
2024-06-17 16:35:28 -0300

    feat: Imprimindo a lista de Clietes

--- src/Banco.java
+++ src/Banco.java
@@ -1,9 +1,25 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class Banco {
     //Atributos
     private String nome;
     private List<Conta> contas;
+
+    public Banco(){
+        this.contas = new ArrayList<>();
+    }
+
+    public void adicionarConta(Conta conta){
+        contas.add(conta);
+    }
+
+    public  void listarClientes(){
+        System.out.println("Clientes: ");
+        for (Conta conta: contas){
+            System.out.println(conta.cliente.getNome());
+        }
+    }
 
     public String getNome() {
         return nome;
--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,27 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Banco banco = new Banco();
+
         Cliente cliente = new Cliente();
         cliente.setNome("Tarcisio");
+        Cliente cliente2 = new Cliente();
+        cliente.setNome("Savio");
 
-        Conta corrente = new ContaCorrente(cliente);
-        Conta poupanca = new ContaPoupanca(cliente);
+        Conta conta1 = new ContaCorrente(cliente);
+        Conta conta2 = new ContaPoupanca(cliente2);
 
-        corrente.Depositar(150.79);
-        corrente.ImprimirExtrato();
+//        corrente.Depositar(150.79);
+//        corrente.ImprimirExtrato();
+//        corrente.Transferir(100, poupanca);
+//        poupanca.ImprimirExtrato();
+//        corrente.ImprimirExtrato();
 
-        corrente.Transferir(100, poupanca);
-        poupanca.ImprimirExtrato();
-        corrente.ImprimirExtrato();
+        banco.adicionarConta(conta1);
+        banco.adicionarConta(conta2);
+
+        banco.listarClientes();
+
+
     }
 }
